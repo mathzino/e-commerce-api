cc0f1687ad2ae1faf9a93ff48651b4a501246776
mathzino <fairusramdhani29@gmail.com>
2022-01-31 11:40:03 +0700

    bb

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 func main() {
     // for load godotenv
     // for env
-    err := godotenv.Load()
+    err := godotenv.Load(".env")
     if err != nil {
         log.Fatal("Error loading .env file")
     }
