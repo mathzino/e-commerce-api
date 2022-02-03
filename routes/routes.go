a8c00279c8feab0bdaa12a69021e03211c76df38
mathzino <fairusramdhani29@gmail.com>
2022-02-03 12:03:34 +0700

    baru

--- routes/routes.go
+++ routes/routes.go
@@ -22,14 +22,6 @@ func SetupRouter(db *gorm.DB) *gin.Engine {
     r.POST("/register", controllers.Register)
     r.POST("/login", controllers.Login)
 
-    // r.GET("/movies", controllers.GetAllMovie)
-    // r.GET("/:id", controllers.GetMovieById)
-
-    // moviesMiddlewareRoute := r.Group("/movies")
-    // moviesMiddlewareRoute.Use(middlewares.JwtAuthMiddleware())
-    // moviesMiddlewareRoute.POST("/", controllers.CreateMovie)
-    // moviesMiddlewareRoute.PATCH("/:id", controllers.UpdateMovie)
-    // moviesMiddlewareRoute.DELETE("/:id", controllers.DeleteMovie)
 //category
     r.GET("/product-categories", controllers.GetAllProductCategory)
     r.GET("/product-categories/:id", controllers.GetCategoryById)
@@ -40,7 +32,7 @@ func SetupRouter(db *gorm.DB) *gin.Engine {
     categoryMiddlewareRoute.POST("/", controllers.CreateCategory)
     categoryMiddlewareRoute.PATCH("/:id", controllers.UpdateCategory)
     categoryMiddlewareRoute.DELETE("/:id", controllers.DeleteProductCategory)
-    //product
+//product
     r.GET("/product", controllers.GetAllProduct)
     r.GET("/product/:id", controllers.GetProductById)
     //with auth
@@ -50,7 +42,7 @@ func SetupRouter(db *gorm.DB) *gin.Engine {
     productMiddlewareRoute.PATCH("/:id", controllers.UpdateProduct)
     productMiddlewareRoute.DELETE("/:id", controllers.DeleteProduct)
     
-    //seller
+//seller
     r.GET("/seller", controllers.GetAllSeleer)
     r.GET("/seller/:id", controllers.GetSellerById)
     //with auth
@@ -59,6 +51,26 @@ func SetupRouter(db *gorm.DB) *gin.Engine {
     SellerMiddlewareRoute.POST("/", controllers.CreateSeller)
     SellerMiddlewareRoute.PATCH("/:id", controllers.UpdateSeller)
     SellerMiddlewareRoute.DELETE("/:id", controllers.DeleteSeller)
+
+    r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+//cart item
+    r.GET("/cart-item", controllers.GetAllCartItem)
+    r.GET("/cart-item/:id", controllers.GetCartItemByIdUser)
+    //with auth
+   CartItemMiddlewareRoute := r.Group("/cart-item")
+   CartItemMiddlewareRoute.Use(middlewares.JwtAuthMiddleware())
+   CartItemMiddlewareRoute.POST("/", controllers.CreateCartItem)
+   CartItemMiddlewareRoute.PATCH("/:id", controllers.UpdateCartItem)
+   CartItemMiddlewareRoute.DELETE("/:id", controllers.DeleteCartItem)
+//Discussion
+    r.GET("/discussion", controllers.GetAllDiscussion)
+    r.GET("/discussion/:id", controllers.GetDiscussionById)
+    //with auth
+   DiscussionMiddlewareRoute := r.Group("/discussion")
+   DiscussionMiddlewareRoute.Use(middlewares.JwtAuthMiddleware())
+   DiscussionMiddlewareRoute.POST("/", controllers.CreateDiscussion)
+   DiscussionMiddlewareRoute.PATCH("/:id", controllers.UpdateDiscussion)
+   DiscussionMiddlewareRoute.DELETE("/:id", controllers.DeleteDiscussion)
 
     r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
